30f70856595d7b9e14ec22a94feec3d28061ffa2
Cedric Chimot <cedric_om10@yahoo.fr>
2024-12-22 10:25:28 +0100

    Toggle menu sur la navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule, RouterOutlet, NavbarComponent],
+  imports: [RouterModule, RouterOutlet, NavbarComponent, NgbCollapseModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
+  imports: [RouterModule, NgbCollapseModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+  toggleNavbar = true;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+  toggleNav() {
+    this.toggleNavbar =!this.toggleNavbar;
+  }
 }

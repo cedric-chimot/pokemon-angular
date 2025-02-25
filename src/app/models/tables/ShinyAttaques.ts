e13fe3748ffd8a095f897e619d71a0736fad8abb
Cedric Chimot <cedric_om10@yahoo.fr>
2025-02-26 00:06:43 +0100

    correction de mise en page

---
+++ src/app/models/tables/ShinyAttaques.ts
@@ -0,0 +1,11 @@
+import { Attaques } from './Attaques';
+import { PokemonShiny } from './PokemonShiny';
+
+export class ShinyAttaques {
+  constructor(
+    public id: number,
+    public pokemonShiny: PokemonShiny,
+    public attaques: Attaques[],
+    public position: number
+  ) {}
+}
